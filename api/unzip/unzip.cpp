5ea9fac6ad1a67dbb7e9a4b0296223d202223e0e
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2022-06-14 09:00:12 +0630

    read and retain zip handle and files offset

--- api/unzip/unzip.cpp
+++ api/unzip/unzip.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <filesystem>
 #include "api/unzip/junzip.h"
 #include "api/unzip/unzip.h"
 #include "lib/json/json.hpp"
 #include "helpers.h"
+
 #include "settings.h"
 #include "resources.h"
 #include "api/filesystem/filesystem.h"
@@ -13,111 +14,129 @@ using namespace std;
 using json = nlohmann::json;
 
 namespace unzip {
-    int processFile(JZFile* zip) {
-        JZFileHeader header;
-        char filename[1024];
-        unsigned char* data;
 
-        if (jzReadLocalFileHeader(zip, &header, filename, sizeof(filename))) {
-            printf("Couldn't read local file header!");
-            return -1;
-        }
-
-        if ((data = (unsigned char*)malloc(*(uint32_t*)header.uncompressedSize)) == NULL) {
-            printf("Couldn't allocate memory!");
-            return -1;
-        }
-
-        printf("  %s, %d / %d bytes at offset %0X\n", filename,
-            *(uint32_t*)header.compressedSize, *(uint32_t*)header.uncompressedSize, header.offset);
-
-        if (jzReadData(zip, &header, data) != Z_OK) {
-            printf("Couldn't read file data!");
-            free(data);
-            return -1;
-        }
-
-        free(data);
-
-        return 0;
+map<string, JZFile *> pool;
+map<string, json > directories;
+    string getZipRoot(){
+    	    string zipRoot;
+            if (resources::getMode() == "directory") {
+                json jZipRoot =settings::getOptionForCurrentMode("zipRoot");
+                if (jZipRoot.is_null()) {
+                    zipRoot = "unzip/";
+                }
+                else {
+                    zipRoot = jZipRoot.get<string>();
+                }
+            }
+            return zipRoot;
     }
-
-string getZipRoot(){
-	string zipRoot;
-        if (resources::getMode() == "directory") {
-            json jZipRoot =settings::getOptionForCurrentMode("zipRoot");
-            if (jZipRoot.is_null()) {
-                zipRoot = "unzip/";
+    void split(std::string& string, std::vector<std::string>& tokens, const char& delim) {
+        std::string ea;
+        std::stringstream stream(string);
+        while (getline(stream, ea, delim))
+            tokens.push_back(ea);
+    }
+    int getFileOffset(json zipdirectory,string filename) {
+        filesystem::path path(filename);
+        string dir = path.parent_path().string();
+        string fn = path.filename().string();
+        int offset = -1;
+        json* directory = &zipdirectory;
+        if (fn != "") { //dig into the 
+            vector <string> folders;
+            split(dir, folders, '/');
+            for (string s : folders) {
+                directory = &((*directory)[s]);
             }
-            else {
-                zipRoot = jZipRoot.get<string>();
+            if ((*directory)[string(fn)].is_number()) {
+                offset = (*directory)[string(fn)];
             }
         }
-        return zipRoot;
-}
- JZFile* openZip(string fn) {
-        FILE* fp;
-        string zipRoot = getZipRoot();
+        return offset;
+    }
+    int directoryCallback(JZFile* zip, int idx, JZFileHeader* header, char* filename, void* _files) {
+        long offset = zip->tell(zip); // store current position
+        auto files = (static_cast<json*>(_files));
+        if (zip->seek(zip, header->offset, SEEK_SET)) {
+            printf("Cannot seek in zip file!");
+            return 0; // abort
+        }
+        filesystem::path path(filename);
+        string dir = path.parent_path().string(); 
+        string fn = path.filename().string(); 
 
-        string zipfn = zipRoot + fn;
-        int err = fopen_s(&fp, zipfn.c_str(), "rb");
-        if (err) return 0;
-        JZFile* zip = jzfile_from_stdio_file(fp);
-        return zip;
+        if (fn != "") { //dig into the 
+            json *directory = files;
+            vector <string> folders;
+            split(dir, folders, '/');
+            for (string s : folders){
+                if ((*directory)[s].is_null()) (*directory)[s] = {};
+                directory = &((*directory)[s]);
+            }
+            (*directory)[string(fn)] = header->offset ;
+        }
+        zip->seek(zip, offset, SEEK_SET); // return to position
+        return 1; // continue
+
+    }
+    json buildDirectory(JZFile *zip) {
+        JZEndRecord endRecord;
+        json files;
+        jzReadEndRecord(zip, &endRecord);
+        jzReadCentralDirectory(zip, &endRecord, directoryCallback, &files);
+
+        return files;
+    }
+    JZFile* openZip(string fn) {
+            auto itr = pool.find(fn);
+            if (itr !=pool.end()) {
+                return itr->second;
+            }
+            FILE* fp;
+            string zipRoot = getZipRoot();
+
+            string zipfn = zipRoot + fn;
+            int err = fopen_s(&fp, zipfn.c_str(), "rb");
+            if (err) return 0;
+            JZFile* zip = jzfile_from_stdio_file(fp);
+            json files= buildDirectory(zip);
+            pool.insert({ fn, zip });
+            directories.insert({ fn,files });
+            return zip;
     }
 
     //file Content must be UTF 8 string
-    string readFileContent(JZFile* zip, string fn) {
+    string readFileContent(JZFile* zip, int offset) {
         JZFileHeader header;
         char filename[1024]; //assuming same with fn
+
+        if (zip->seek(zip, offset, SEEK_SET)) {
+            printf("Cannot seek in zip file!");
+            return ""; // abort
+        }
+
         if (jzReadLocalFileHeader(zip, &header, filename, sizeof(filename))) return "";  
         int size = *(uint32_t*)header.uncompressedSize;
         char*  data = (char*) malloc(1 + size );
-        *(data+size) = '\0';
+        *(data+size) = 0;
         if (jzReadData(zip, &header, data) != Z_OK) {
             printf("Couldn't read file data!");
             free(data);
             return "";
         }
-        string out = (string)(data);
+        string out = data;
         free(data);
         return out;
     }
 
-    int findFileCallback(JZFile* zip, int idx, JZFileHeader* header, char* filename, void* _file) {
-        long offset = zip->tell(zip); // store current position
-        auto file = (static_cast<json*>(_file));
 
-        if ((string)filename == (*file)["path"].get<string>()) {
-            if (zip->seek(zip, header->offset, SEEK_SET)) return 0;
-            (*file)["found"] = true;
-            (*file)["content"]= readFileContent(zip, (string)filename);
-            return 0;
-        }
-        zip->seek(zip, offset, SEEK_SET); // return to position
-        return 1; // continue
-    }
-    int fileListingCallback(JZFile* zip, int idx, JZFileHeader* header, char* filename, void* _files) {
-        long offset = zip->tell(zip); // store current position
-        auto files = (static_cast<json*>(_files));
-
-        if (zip->seek(zip, header->offset, SEEK_SET)) {
-            printf("Cannot seek in zip file!");
-            return 0; // abort
-        }
-        int size = *(uint32_t*)header->uncompressedSize; // need casting  , stupid patch on windows
-        (*files)[string(filename)] = { {"size"  , size}, {"idx",idx} };  //just return the uncompressedSize and idx in zip
-        //processFile(zip); // alters file offset
-        zip->seek(zip, offset, SEEK_SET); // return to position
-        return 1; // continue
-    }
 namespace controllers {
 
 static bool endsWith(const std::string& str, const std::string& suffix)
 {
     return str.size() >= suffix.size() && 0 == str.compare(str.size() - suffix.size(), suffix.size(), suffix);
 }
-json zipList(const json& input) { //list all available 
+json zipList(const json& input) { //list all zip files in zip folder
 	json ip;
     string zipRoot= getZipRoot();
     ip["path"] = zipRoot;
@@ -146,33 +165,29 @@ json readFile(const json& input) {
 
     string zipfn = input["zip"].get<string>();
     JZFile* zip = openZip(zipfn);
+
     if (!zip) {
         output["error"] = helpers::makeErrorPayload("NE_FS_FILRDER", "cannot open zip " + zipfn);
         return output;
     }
-    JZEndRecord endRecord;
-    jzReadEndRecord(zip, &endRecord);
-
-    string path = input["path"].get < string>();
-    json file;
-    file["path"] = path;
-
-    jzReadCentralDirectory(zip, &endRecord, findFileCallback, &file );
-    zip->close(zip);
-    if (file["found"].get<bool>()) {
-        string str = file["content"].get<string>();
-        output["returnValue"] = str;
-        output["success"] = true;
-    } else {
-        output["error"]= helpers::makeErrorPayload("NE_FS_FILRDER", "cannot open file in zip " + path);
+    string path = input["path"].get<string>();
+    auto itr = directories.find(zipfn);
+    json directory=itr->second;
+    int offset = getFileOffset(directory, path);
+    if (offset >= 0) {
+        string content = readFileContent(zip, offset);
+        if (content != "") {
+            output["returnValue"] = content;
+            output["success"] = true;
+            return output;
+        }
     }
+    output["error"]= helpers::makeErrorPayload("NE_FS_FILRDER", "cannot open file in zip " + path);
+    
     return output;
 }
 json fileList(const json &input){
     json output;
-    
-    JZEndRecord endRecord;
-    
     if( !helpers::hasRequiredFields(input, {"zip"})  ) {
         output["error"] = helpers::makeMissingArgErrorPayload();
         return output;
@@ -185,12 +200,14 @@ json fileList(const json &input){
     	 output["error"] =helpers::makeErrorPayload("NE_FS_FILRDER", "cannot open zip " + zipfn);
         return output;
    }
-    jzReadEndRecord(zip, &endRecord);   
-    json files;
-    jzReadCentralDirectory(zip, &endRecord, fileListingCallback, &files);
-    zip->close(zip);
-    output["returnValue"] = files;
-    output["success"] = true;
+    auto itr = directories.find(zipfn);
+    if (itr != directories.end()) {
+        output["returnValue"] = itr->second;
+        output["success"] = true;
+    }
+    else {
+        output["error"] = helpers::makeErrorPayload("NE_FS_FILRDER", "cannot get directories in zip");
+    }
     return output;
 }
 
